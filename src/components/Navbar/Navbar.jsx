1f1692bf662693db102cb188d8ca5d076e1fe730
PedroErnestoDev <ernestopedro284@gmail.com>
2025-09-02 07:11:15 -0300

    Adicionando fotos ao assests

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,11 @@
+import "./Navbar.sass"
+
+export default function Navbar(){
+    return (
+        <>
+            <nav className="navbar">
+                <h1>oi</h1>
+            </nav>
+        </>
+    )
+}
